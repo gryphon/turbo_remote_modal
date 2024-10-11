bd1ad687be87ffd09c8b4847dc9338f1f53783c9
Grigory Dmitrenko <mail@grigor.io>
2024-10-11 16:13:56 +0300

    Auto-load helper

--- lib/turbo_remote_modal/engine.rb
+++ lib/turbo_remote_modal/engine.rb
@@ -7,6 +7,12 @@ module TurboRemoteModal
       g.fixture_replacement :factory_bot
       g.factory_bot dir: 'spec/factories'
     end
+    
+    initializer 'action_controller.include_concern' do
+      ActiveSupport.on_load(:action_controller) do
+        helper TurboRemoteModal::ApplicationHelper
+      end
+    end
 
   end
 end
